4f41a1e3fe1c844595424cea1680bd12e1e72944
Arpit Agarwal <57696942+ArpitAgarwal1998@users.noreply.github.com>
2020-05-31 19:55:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//jshint esversion: 8
 $(document).ready(function(){
 
   $('a.nav-link').on('click',function(event){
@@ -22,3 +23,44 @@ function myFunction() {
     x.className = "navbar";
   }
 }
+
+//---------------------------------------------------NAVBAR HIGHLIGHTING------------------------------------
+
+var nextValue, temp;
+var section_ids = $('*[id^="section"]').splice(1,);
+var links = $('.nav-link').splice(0,section_ids.length);
+$(window).scroll(function()
+{
+    let scrollValue = Number($(window).scrollTop());
+    for(i=0;i<section_ids.length;i++)
+    {
+        let sectionValue = Number(section_ids[i].offsetTop-1);
+        if((i+1)<section_ids.length)
+        {
+           nextValue = Number(section_ids[i+1].offsetTop-1);
+        }
+        else
+        {
+           nextValue = scrollValue+1;
+        }
+        if ((scrollValue >= sectionValue)&&(scrollValue<nextValue))
+        {
+            var idValue = section_ids[i].id;
+            idValue = idValue[0] + idValue[idValue.length-1];
+            for(let value of links)
+            {
+                if (value.classList.contains('active'))
+                {
+                    value.classList.remove('active');
+                }
+                if (idValue == value.id)
+                {
+                    value.classList.add('active');
+                }
+
+            }
+
+        }
+
+    }
+});
